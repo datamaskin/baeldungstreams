d8835f600812361e10d056f18c899db4c8badd19
david <david@davidwbrown.name>
2019-05-14 19:32:10 -0500

    All new to learn Java Stream API Baeldung

---
+++ src/main/java/baeldungstreams/App.java
@@ -0,0 +1,13 @@
+package baeldungstreams;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
